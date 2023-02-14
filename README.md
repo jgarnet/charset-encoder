ee1d1d93821a4dc69023321c9df48e61131718f3
Jalen Garnett <jgarnet@Jalens-MacBook-Pro.local>
2023-02-14 08:53:58 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,83 @@
+# Charset Encoder
+Allows messages to be encoded and decoded using a provided charset. Randomized key:value pairs are used for encoding and can be exported/imported.
+
+Mappings can be exported and imported to use for encoding and decoding messages using the format `k0:v0,k1:v1,...kN:vN` where:
+
+- `N` is the total number of characters in the charset
+- the charset is represented using integer indexes corresponding to each character in the set (i.e. from index 0 ... index N)
+- a given `k` value is an index corresponding to a character in the charset, representing a 'key' entry in the mapping
+- a given `v` value is an index corresponding to a character in the charset, representing a 'value' entry in the mapping
+- a given `k:v` pair represents an inverse relationship between two characters in the charset (`charset[k] <-> charset[v]`)
+
+For example, given a charset `abc` (`a -> 0, b -> 1, c -> 2`), the following mapping may be generated:
+
+`0:2,1:0,2:1` meaning `a -> c, b -> a, c -> b` during encoding.
+
+This mapping would result in a given message `cab` being encoded as `bca` (and consequently `bca` would decode as `cab`).
+
+## Use
+To use CharsetEncoder, a charset must be provided in the constructor's `options` parameter:
+
+```javascript
+const charsetEncoder = new CharsetEncoder({
+    charset: 'abcdefghijklmnopqrstuvwxyz'
+});
+```
+
+Messages can be encoded & decoded:
+
+```javascript
+const charsetEncoder = new CharsetEncoder({
+    charset: 'abcdefghijklmnopqrstuvwxyz'
+});
+
+const encoded = charsetEncoder.encode('test');
+console.log(encoded);
+// example output --> erye
+
+const decoded = charsetEncoder.decode(encoded);
+console.log(decoded);
+// output --> test
+```
+
+A charset can contain letters, numbers, and special characters -- for example, the following are all valid charsets:
+
+```javascript
+const charsetEncoderAlphabet = new CharsetEncoder({
+    charset: 'abcdefghijklmnopqrstuvwxyz'
+});
+
+const charsetEncoderAlphanumeric = new CharsetEncoder({
+    charset: 'abcdefghijklmnopqrstuvwxyz0123456789'
+});
+
+const charsetEncoderCustom = new CharsetEncoder({
+    charset: '1234567890!@#$'
+});
+```
+A predetermined mapping can be passed as an optional parameter to CharsetEncoder:
+
+```javascript
+const charsetEncoder = new CharsetEncoder({
+    charset: 'abc',
+    mapping: '0:2,1:0,2:1'
+});
+
+const encoded = charsetEncoder.encode('cab');
+console.log(encoded);
+// output -> bca
+
+const decoded = charsetEncoder.decode('bca');
+console.log(decoded);
+// output -> abc
+```
+
+Mappings can be exported for later use:
+```javascript
+const charsetEncoder = new CharsetEncoder({
+    charset: 'abc'
+});
+const mapping = charsetEncoder.exportMapping();
+console.log(mapping);
+// example output -> 0:2,1:0,2:1
+```
---
+++ src/charset-encoder.js
@@ -0,0 +1,138 @@
+/**
+ * Encodes & decodes messages based on a supplied character set
+ */
+export default class CharsetEncoder {
+
+    /**
+     * Initializes CharsetEncoder.
+     * If 'map' is supplied as an option, CharsetEncoder will be initialized based on the provided mapped values.
+     * @param options
+     */
+    constructor(options) {
+        if (!options || !options.charset) {
+            throw new Error('CharsetEncoder requires a charset to initialize');
+        }
+        this.setCharset(options.charset);
+        if (options.mapping) {
+            this.importMapping(options.mapping);
+        } else {
+            this.initializeMaps();
+        }
+    }
+
+    setCharset(charset) {
+        const chars = new Map();
+        for (const c of charset) {
+            if (chars.has(c)) {
+                throw new Error('Charset cannot contain duplicate values');
+            }
+            chars.set(c, true);
+        }
+        this.charset = charset;
+    }
+
+    /**
+     * Imports Map(N) from String with format key1:value1,key2:value2,...keyN:valueN.
+     * Keys & Values must be numeric values.
+     * @param mapStr
+     */
+    importMapping(mapStr) {
+        // ensure mapping only contains numeric values, colons & commas
+        if (/[^\d:,]/g.test(mapStr)) {
+            throw new Error('Mapping contains invalid values');
+        }
+        // ensure number of pairs matches charset length
+        const numberOfPairs = (mapStr.match(/\d+:\d+/g) || []).length;
+        if (numberOfPairs !== this.charset.length) {
+            throw new Error('Mapping does not match charset');
+        }
+        this.encodeMap = new Map();
+        this.decodeMap = new Map();
+        const entries = mapStr.split(',');
+        for (const entry of entries) {
+            const parts = entry.split(':');
+            const key = Number(parts[0]);
+            const value = Number(parts[1]);
+            if (key > this.charset.length || value > this.charset.length) {
+                throw new Error('Mapping cannot contain invalid indexes');
+            }
+            if (key === value) {
+                throw new Error('Mapping cannot contain self-references');
+            }
+            // disallow duplicate mappings (i.e. 0:1,...23:1 or 0:1,0:2)
+            if (this.encodeMap.has(key) || this.decodeMap.has(value)) {
+                throw new Error('Mapping cannot contain duplicate references');
+            }
+            this.encodeMap.set(key, value);
+            this.decodeMap.set(value, key);
+        }
+    }
+
+    /**
+     * Exports Map(N) as String with format key1:value1,key2:value2,...keyN:valueN.
+     * Keys & Values will be numeric values.
+     * @returns {string}
+     */
+    exportMapping() {
+        let result = '';
+        for (const entry of this.encodeMap.entries()) {
+            result += `${entry[0]}:${entry[1]},`;
+        }
+        return result.substr(0, result.length - 1);
+    }
+
+    /**
+     * Initializes internal encodeMap & decodeMap based on supplied charset.
+     * Generates random key:value pairs to be used for encoding & decoding messages.
+     */
+    initializeMaps() {
+        this.encodeMap = new Map();
+        this.decodeMap = new Map();
+        // used to keep track of available potential key:value pairs as encode / decode Maps are populated
+        const unusedKeys = [];
+        const unusedValues = [];
+        // keep track of possible indexes in charset
+        for (let i = 0; i < this.charset.length; i++) {
+            unusedKeys.push(i);
+            unusedValues.push(i);
+        }
+        // populate encodeMap & decodeMap based on possible random unique pairs
+        while (this.encodeMap.size < this.charset.length) {
+            const key = unusedKeys[0];
+            const randomIndex = this.getRandom(unusedValues.length);
+            const value = unusedValues[randomIndex];
+            // skip iteration if key === value (self-reference) or key/value is already in a pair (collision)
+            const shouldContinue = key === value || this.encodeMap.has(key) || this.decodeMap.has(value);
+            if (shouldContinue) {
+                continue;
+            }
+            this.encodeMap.set(key, value);
+            this.decodeMap.set(value, key);
+            // remove key & value from unusedKeys & unusedValues since they have been paired together
+            unusedKeys.splice(0, 1);
+            unusedValues.splice(randomIndex, 1);
+        }
+    }
+
+    getRandom(max) {
+        return Math.floor(Math.random() * max);
+    }
+
+    encode(msg) {
+        return this.translate(msg, this.encodeMap);
+    }
+
+    decode(msg) {
+        return this.translate(msg, this.decodeMap);
+    }
+
+    translate(msg, map) {
+        let result = '';
+        for (const c of msg) {
+            const index = this.charset.indexOf(c);
+            result += index !== -1 ? this.charset.charAt(map.get(index)) : c;
+        }
+        return result;
+    }
+
+}
